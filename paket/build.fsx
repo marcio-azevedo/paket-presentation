656348610bfa8d76071233497aad6588e9cb0c32
Márcio Azevedo <marcio.azevedo@basecone.nl>
2017-05-29 15:14:17 +0100

    updated build and paket bootstrapper.

--- paket/build.fsx
+++ paket/build.fsx
@@ -12,7 +12,7 @@
 let gitOwner = "marcio-azevedo"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "presentations"
+let gitProjectName = "marcio-azevedo.github.io"
 // The name of the GitHub repo subdirectory to publish slides to
 let gitSubDir = "paket"
 
