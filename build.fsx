3d4865d8186061d32b1d4b1e80e7b50b10149a2f
Márcio Azevedo <marcio.azevedo@basecone.nl>
2017-05-30 10:47:51 +0100

    updated release folder

--- build.fsx
+++ build.fsx
@@ -14,7 +14,7 @@ let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
 let gitProjectName = "paket-presentation"
 // The name of the GitHub repo subdirectory to publish slides to
-let gitSubDir = "output"
+let gitSubDir = "slides-released"
 
 open FsReveal
 open Fake
